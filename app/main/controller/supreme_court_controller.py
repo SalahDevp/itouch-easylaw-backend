b234ada457db75e36165703a07c93439136bdc19
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-05-18 21:18:42 +0100

    Add update supreme court decisions

--- app/main/controller/supreme_court_controller.py
+++ app/main/controller/supreme_court_controller.py
@@ -1,8 +1,10 @@
 from flask_restx import Resource
+from app.main.service.supreme_court_service import SupremeCourtService
 from http import HTTPStatus
 from app.main.service.search_service import SearchService
 from flask import request
 from app.main.controller.dto.supreme_court_dto import SupremeCourtDto
+from app.main.decorators.auth_decorators import require_authentication, allow_roles
 
 api = SupremeCourtDto.api
 search_service = SearchService()
@@ -43,3 +45,24 @@ class SupremeCourt(Resource):
             ),
             HTTPStatus.OK,
         )
+
+
+@api.route("/<decision_id>")
+class SupremeCourtDetails(Resource):
+    @api.response(
+        HTTPStatus.OK,
+        description="Get a decision by id",
+        model=SupremeCourtDto.case_model,
+    )
+    def get(self, decision_id: str):
+        return SupremeCourtService().get_decision_by_id(decision_id), HTTPStatus.OK
+
+    @api.doc(description="Update a decision\nPermissions: moderator")
+    @api.expect(SupremeCourtDto.update_decision_request, validate=True)
+    @api.response(HTTPStatus.NO_CONTENT, description="Success")
+    @require_authentication
+    @allow_roles(["moderator"])
+    def put(self, decision_id: str):
+        data = api.payload
+        SupremeCourtService().update_decision(decision_id, data)
+        return "", HTTPStatus.NO_CONTENT
